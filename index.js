d2af325e4568cc4214e590fc2950448036030781
Daniel Navarro <daniel.navarro@newtech.consulting>
2025-06-05 12:44:19 +0200

    mix

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const youtubesearchapi = require('youtube-search-api');
 const path = require('path');
 const fs = require('fs');
 const { ytmp3, ytmp4 } = require('@vreden/youtube_scraper');
-const { default: axios } = require('axios')
+const { default: axios } = require('axios');
+const { log } = require('console');
 const MAX_LENGTH = 3000
 const prompt = "Eres un bot de discord llamado Tbz bot, estas aqui para ayudar a los integrantes del grupo, siempre responde en español. "
 
@@ -18,6 +19,43 @@ function getListIdFromUrl(url) {
   const urlObj = new URL(url);
   return urlObj.searchParams.get("list");
 }
+
+const GetYoutubeMixData = async (url) => {
+  var initdata = await {};
+  var apiToken = await null;
+  var context = await null;
+  try {
+    const page = await axios.get(encodeURI(url));
+    const ytInitData = await page.data.split("var ytInitialData =");
+    if (ytInitData && ytInitData.length > 1) {
+      const data = await ytInitData[1].split("</script>")[0].slice(0, -1);
+
+      if (page.data.split("innertubeApiKey").length > 0) {
+        apiToken = await page.data
+          .split("innertubeApiKey")[1]
+          .trim()
+          .split(",")[0]
+          .split('"')[2];
+      }
+
+      if (page.data.split("INNERTUBE_CONTEXT").length > 0) {
+        context = await JSON.parse(
+          page.data.split("INNERTUBE_CONTEXT")[1].trim().slice(2, -2)
+        );
+      }
+
+      initdata = await JSON.parse(data);
+      
+      return await Promise.resolve({ initdata, apiToken, context, mixItems: initdata?.contents?.twoColumnWatchNextResults?.playlist?.playlist?.contents[0]?.playlistPanelVideoRenderer || [] });
+    } else {
+      console.error("cannot_get_init_data");
+      return await Promise.reject("cannot_get_init_data");
+    }
+  } catch (ex) {
+    console.error(ex);
+    return await Promise.reject(ex);
+  }
+};
 
 async function downloadAudioFromYouTube(url, filePath) {
     try {
@@ -305,6 +343,86 @@ Client.on('messageCreate', async message => {
                         .setColor('Purple')
                         .setTitle(item.title)
                         .setDescription(item.length.simpleText)
+                        .setThumbnail(item.thumbnail.thumbnails[0]?.url)
+    
+                    await message.reply({ embeds: [embed]});
+    
+                    // Esperar a que la canción actual termine de reproducirse
+                    await songEndPromise;
+    
+                    // Destruir la conexión de voz antes de reproducir la siguiente canción si es la última en la lista
+                    if (i === playlistItems.length - 1) {
+                        connection.destroy();
+                    }
+                }
+    
+                message.reply('La lista de reproducción ha sido reproducida.');
+            } else {
+                message.reply('¡Debes estar en un canal de voz para que el bot se una!');
+            }
+        } catch (error) {
+            console.error('Error al unirse al canal de voz:', error);
+            message.reply('Hubo un error al reproducir la lista de reproducción.');
+        }
+    }
+
+    if(message.content.startsWith('!playmix')) {
+        const args = message.content.split(' ');
+        args.shift(); // Remueve !play del array
+        const url = args.join(' ');
+
+        try {
+            if (!url) return message.reply('Por favor, proporciona una URL de mix válida de YouTube.');
+
+            if (message.guild && message.member?.voice?.channelId) {
+                const connection = joinVoiceChannel({
+                    channelId: message.member?.voice?.channel?.id,
+                    guildId: message.guildId,
+                    adapterCreator: message.guild.voiceAdapterCreator
+                });
+                message.reply('Buscando...');
+                // Buscar en YouTube
+                
+                const mixInfo = await GetYoutubeMixData(url);
+                
+                const playlistItems = mixInfo.mixItems
+
+                console.log(playlistItems);
+                
+                
+                if (playlistItems.length === 0) {
+                    message.reply('El mix de reproducción está vacío.');
+                    return;
+                }
+    
+                for (let i = 0; i < playlistItems.length; i++) {
+                    const item = playlistItems[i];
+                    const videoId = item.videoId;
+                    console.log(videoId);
+                    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    
+                    const filePath = path.join(__dirname, `audio.mp3`);
+                    await downloadAudioFromYouTube(videoUrl, filePath);
+    
+                    const resource = createAudioResource(filePath);
+                    const player = createAudioPlayer();
+    
+                    // Crear una promesa para resolver cuando la canción termine de reproducirse
+                    const songEndPromise = new Promise(resolve => {
+                        player.on('stateChange', (oldState, newState) => {
+                            if (newState.status === 'idle') {
+                                resolve();
+                            }
+                        });
+                    });
+    
+                    player.play(resource);
+                    connection.subscribe(player);
+
+                    const embed = new EmbedBuilder()
+                        .setColor('Purple')
+                        .setTitle(item.title.simpleText)
+                        .setDescription(item.lengthText.simpleText)
                         .setThumbnail(item.thumbnail.thumbnails[0]?.url)
     
                     await message.reply({ embeds: [embed]});
